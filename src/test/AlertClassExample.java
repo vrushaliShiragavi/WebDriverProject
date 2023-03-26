1c281167fb31a61ad45400d42f0cc771c6208025
vrushaliShiragavi <vrushali.shiragavi@gmail.com>
2023-03-26 10:21:31 +0530

    second Commit for class rename

--- src/test/AlertClassExample.java
+++ src/test/AlertClassExample.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AlertClassExampple {
+public class AlertClassExample {
 
 	public static void main(String[] args) throws InterruptedException {
 
